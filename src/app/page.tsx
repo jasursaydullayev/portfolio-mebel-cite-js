e0dc81d9d54abdfaf9e53d90c4b1eabb03f34249
Jasurbek1810 <JasurbekSaydullayev@gmail.com>
2024-03-22 16:22:13 +0500

    end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
-import About from "@/components/About"
-import BottomNav from "@/components/BottomNav"
-import Compnay from "@/components/Compnay"
-import Furniture from "@/components/Furniture"
-import Navbar from "@/components/Navbar"
+import About from "@/components/About";
+import AllFurniture from "@/components/AllFurniture";
+import BottomNav from "@/components/BottomNav";
+import Compnay from "@/components/Compnay";
+import Footer from "@/components/Footer";
+import Furniture from "@/components/Furniture";
+import Navbar from "@/components/Navbar";
+import TopFooter from "@/components/TopFooter";
 function page() {
   return (
-    <div className="container">
+    <div>
+      <main className="container">
       <Navbar />
-      <BottomNav />
-      <Compnay />
-      <About />
-      <Furniture />
+        <BottomNav />
+        <Compnay />
+        <About />
+        <Furniture />
+        <AllFurniture />
+        <TopFooter />
+      </main>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
